b40ab00ce41612da61e403553131514d55c3b7ed
Ryan Blackman <rblackman24@gmail.com>
2026-01-01 09:12:44 -0600

    feature: prep for monorepo

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,13 +1,12 @@
+// ./apps/website/next.config.js
+import path from "node:path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	outputFileTracingRoot: path.join(__dirname, "../../"),
 	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "cdn.sanity.io",
-			},
-		],
+		remotePatterns: [{ protocol: "https", hostname: "cdn.sanity.io" }],
 	},
 };
 
