ccbb999de301bbb9ec67b98d5aba13ea39db6afd
Ryan Blackman <rblackman24@gmail.com>
2026-01-03 08:37:37 -0600

    chore: format

--- apps/website/app/recipe/features/secondary-featured-recipes/index.tsx
+++ apps/website/app/recipe/features/secondary-featured-recipes/index.tsx
@@ -19,7 +19,11 @@ export default function SecondaryFeaturedRecipes({ recipes }: Props) {
 			</Heading>
 			<div className={styles.secondaryGrid}>
 				{recipes.map(({ recipe, index }) => (
-					<SecondaryFeaturedRecipe key={recipe._id ? `${recipe._id}-${index}` : `idx-${index}`} recipe={recipe} index={index} />
+					<SecondaryFeaturedRecipe
+						key={recipe._id ? `${recipe._id}-${index}` : `idx-${index}`}
+						recipe={recipe}
+						index={index}
+					/>
 				))}
 			</div>
 		</>
