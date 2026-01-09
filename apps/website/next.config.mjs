ac84644f13b9eda96d1931397b9e2e64e9360315
Ryan Blackman <rblackman@mtd.org>
2026-01-09 17:07:11 -0600

    chore: lint

--- apps/website/next.config.mjs
+++ apps/website/next.config.mjs
@@ -1,6 +1,6 @@
-import createWithVercelToolbar from "@vercel/toolbar/plugins/next";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import createWithVercelToolbar from "@vercel/toolbar/plugins/next";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
