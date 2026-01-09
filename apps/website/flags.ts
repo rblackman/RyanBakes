f8726ac2c1e7ae1dcc3f34b87ce21c7276cd05b7
Ryan Blackman <rblackman@mtd.org>
2026-01-09 16:55:29 -0600

    bug: trying to fix flags

--- apps/website/flags.ts
+++ apps/website/flags.ts
@@ -1,7 +1,10 @@
 import { flag } from "flags/next";
 
+const origin = process.env.FLAGS_ORIGIN;
+
 export const darkModeOverride = flag<boolean>({
 	key: "dark-mode-override",
 	decide: () => false,
 	description: "Override to enable dark mode for all users",
+	...(origin ? { origin } : {}),
 });
